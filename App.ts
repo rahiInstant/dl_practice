a086c3ccb8f119c60e2aba9a5d10a5247c6111f1
rahiinstant <rahiurp20@gmail.com>
2026-02-26 16:17:15 +0600

    add expected output

--- App.ts
+++ App.ts
@@ -4,3 +4,12 @@ const cacheService = new CacheService();
 cacheService.set("view", "300");
 cacheService.connect().set("amount", "5000");
 cacheService.put().set("ItemName", "phone");
+
+// ========== output
+// value set to RedishCache view 300
+// connet to MemCache
+// value set to MemCache amount 5000
+// put to InMemoryCache
+// value set to InMemoryCache ItemName phone
+
+
